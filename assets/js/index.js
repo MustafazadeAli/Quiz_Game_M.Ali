dee315792e7b8a8d692996e87fac971db5da4463
MustafazadeAli <honorcomputer14@gmail.com>
2026-02-06 18:31:02 +0400

    Added js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,13 @@
-let game_page = document.querySelector(".game_page");
+let gamePage = document.querySelector(".gamePage");
 let progressValue = 0;
 let randomIndex = 0;
 let clickEnabled = true;
-
+let counter = 1;
+const keyToastEl = document.getElementById("keyToast")
+const keyToast = new bootstrap.Toast(keyToastEl,{
+  autohide: true,
+  delay: 10000
+})
 let questions = [
   {
     question: "Inside which HTML element do we put the JavaScript?",
@@ -39,13 +44,97 @@ class QuizGame {
   getRandomQuestion() {
     randomIndex = Math.floor(Math.random() * questions.length);
   }
+  showQuestion() {
+    gamePage_container = document.createElement("div");
+    gamePage_container.className = "container gamePage_container";
+    gamePage_container.innerHTML = `
+     <div class="container">
+        <div>
+          <div class="progressBarContainer">
+            <div class="progress">
+              <div
+                class="progress-bar"
+                role="progressbar"
+                aria-label="Example with label"
+                style="width:" + ${progressValue} "%"
+                aria-valuenow="${progressValue}"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              >
+               ${progressValue === 0 ? "" : progressValue}
+              </div>
+            </div>
+          </div>
+          <div class="questionSection">
+            <div>
+              <div class="title">Question ${counter}</div>
+              <div class="desc "> ${questions[randomIndex].question}
+              </div>
+            </div>
+          </div>
+          <div class="row answerSection">
+            <div class="col-12">
+              <div>A)</div>
+              <div id="a" >${questions[randomIndex].answers[0]}</div>
+            </div>
+            <div class="col-12">
+              <div>B)</div>
+              <div id="b">${questions[randomIndex].answers[1]}</div>
+            </div>
+            <div class="col-12">
+              <div>C)</div>
+              <div id="c">${questions[randomIndex].answers[2]}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    
+    `;
+  }
 }
 
 let newGame = new QuizGame();
-document.querySelector(".startButton").addEventListener("click", (e) => {
+
+
+window.addEventListener(
+  "keydown",
+  (a = (e) => {
+    if (clickEnabled) {
+      if (e.key === "a" || e.key === "b" || e.key === "c") {
+        clickEnabled = false;
+        if (e.key === questions[randomIndex].trueAnswer) {
+          let progress_bar = document.querySelector(".progress-bar");
+          let faiz = progressValue + Math.floor(100 / questions.length);
+          progress_bar.style.width = faiz + "%";
+          progress_bar.textContent = faiz + "%"; // Görselde innerHTML kullanılmış
+          document.querySelector("#" + e.key).style.backgroundColor = "#163d08";
+          document.querySelector("#" + e.key).style.color = "white";
+        } else {
+          document.querySelector("#" + e.key).style.backgroundColor = "#8f1919";
+          document.querySelector("#" + e.key).style.color = "white";
+        }
+
+        setTimeout(() => {
+          newGame.nextQuestion();
+          clickEnabled = true;
+        }, 1000);
+      } else {
+        if (!keyToastEl.classList.contains("show")) {
+          keyToast.show();
+        }
+      }
+    } else {
+      console.log("Warning !");
+    }
+  })
+);
+
+
+document.querySelector(".startButton").addEventListener("click", () => {
   document.querySelector(".firstPage").classList.add("hide");
   document.querySelector(".gamePage").classList.add("show");
 });
-document.querySelector(".exitButton").addEventListener("click", (e) => {
+document.querySelector(".exitButton").addEventListener("click", () => {
   window.close();
 });
